56de859fa3d4cbc0b2aaee82470be581187d6971
Muhammad Junaid Rehman Siddiqui <jbhai8058@gmail.com>
2023-06-20 13:21:06 +0500

    =

--- src/Component/Item/Item.jsx
+++ src/Component/Item/Item.jsx
@@ -1,35 +1,38 @@
-import React, { Fragment } from 'react'
-import { Link, Route, BrowserRouter as Router, Routes, } from 'react-router-dom'
+import React, { Fragment, useState } from 'react'
+import { Nav } from 'react-bootstrap';
+import { Link, Route, BrowserRouter as Router, Routes, } from 'react-router-dom';
+import Pill1 from './pill1';
 
 const Item = () => {
 
-  const AddUpdate = () => {
-    return (
-      <div className="tab-pane active">
-        <h1>item</h1>
-      </div>
-    );
+  const [activeKey, setActiveKey] = useState("1");
+
+  const handleSelect = (selectedKey) => {
+    setActiveKey(selectedKey);
   };
 
-  const List = () => {
-    return (
-      <div className="tab-pane fade">
-        <h1>search</h1>
-      </div>
-    );
+
+  // Define content for each nav pills key
+  const contentMap = {
+    "1": <Pill1 />,
+    "2": "Content for Pill 2",
+    "3": "Content for Pill 3",
   };
+
+  // Get the content based on the active key
+  const activeContent = contentMap[activeKey];
 
   return (
     <Fragment>
       <div className="row">
-        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div className="col-sm-12 col-md-12 col-lg-12">
           <div className="container-fluid">
             <div className="w-full p-3 bg-white border border-gray-200 rounded-lg mt-2">
               <div className="row">
-                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div className="col-sm-12 col-md-12 col-lg-4">
                   <h1 className='text-sm text-md text-lg font-weight-bold text-uppercase'>Add Item</h1>
                 </div>
-                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
+                <div className="col-sm-12 col-md-12 col-lg-8">
                   <div className="float-right"></div>
                 </div>
               </div>
@@ -38,30 +41,28 @@ const Item = () => {
         </div>
       </div>
       <div className="row mt-2">
-        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div className="col-sm-12 col-md-12 col-lg-12">
           <div className="container-fluid">
             <div className="card">
               <div className="card-header">
                 <div className="row">
-                  <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                    <ul className='nav nav-pills'>
-                      <li className='nav-item'><Link className='nav-link active' to='/additem'>Add / Update</Link></li>
-                      <li className='nav-item'><Link className='nav-link' to='/search'>List</Link></li>
-                    </ul>
+                  <div className="col-sm-12 col-md-12 col-lg-12">
+                    <Nav variant="pills" activeKey={activeKey} onSelect={handleSelect}>
+                      <Nav.Item>
+                        <Nav.Link eventKey="1">Pill 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="2">Pill 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="3">Pill 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
                   </div>
                 </div>
               </div>
               <div className="card-body">
-                <div className="row">
-                  <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                    <div className="tab-content">
-                      <Routes>
-                        <Route path="/additem" className="tab-pane active" element={<AddUpdate />} />
-                        <Route path="/search" className="tab-pane fade" element={<List />} />
-                      </Routes>
-                    </div>
-                  </div>
-                </div>
+                {activeContent}
               </div>
             </div>
           </div>
---
+++ src/Component/Item/pill1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pill1 = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Pill1
